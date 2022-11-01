770c115f5239087b3ae509746160fcee02b48aa4
soooyaya <hsoo0531@naver.com>
2022-11-01 16:55:00 +0900

    03-p

--- main.c
+++ main.c
@@ -7,18 +7,22 @@
 int main(int argc, char *argv[]) {
 	
 	int i;
-	int grade[SIZE];
-	int score[SIZE];
+	int a[SIZE] = {1,2,3,4,5};
+	int b[SIZE] = {1,2,3,4,5};
+	int flag = 0;
 	
 	for (i=0;i<SIZE;i++)
-	   grade[i]= rand() % 100;
-	
-	for (i=0;i<SIZE;i++)   
-	   score[i] = grade[i];   
-	      
-	for (i=0;i<SIZE;i++)
-	   printf("score[%d] = %d (%d)\n", i, score[i], grade[i]);
-	   
-	      
+	{
+		if (a[i] != b[i])
+		{ 
+		    flag = 1;
+		    printf("a is not the same with b.\n");
+		    break;
+		}
+	}
+	     
+	if(flag == 0)
+		printf("a is the same with b.\n");
+		 
 	return 0;
 }
