8fd86a395caba8d21f88d97b2b1ce3054feae879
soooyaya <hsoo0531@naver.com>
2022-11-01 17:03:10 +0900

    04

--- main.c
+++ main.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 5
+#define SIZE 4
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void square_array(int a[], int size);
+void print_array(int a[], int size);
+
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	int a[SIZE] = {1,2,3,4,5};
-	int b[SIZE] = {1,2,3,4,5};
-	int flag = 0;
+	int list[SIZE]	= {1, 2, 3, 4};
 	
-	for (i=0;i<SIZE;i++)
-	{
-		if (a[i] != b[i])
-		{ 
-		    flag = 1;
-		    printf("a is not the same with b.\n");
-		    break;
-		}
-	}
-	     
-	if(flag == 0)
-		printf("a is the same with b.\n");
-		 
+	print_array(list, SIZE);
+	square_array(list, SIZE);
+	print_array(list, SIZE);
+	
 	return 0;
+}
+
+void square_array(int a[], int size)
+{ 
+  int i;
+
+  for (i=0;i<size;i++)
+     a[i] = a[i]*a[i];
+}
+
+void print_array(int a[], int size)
+{
+	int i;
+	
+	for (i=0;i<size;i++)
+	  printf("%3d", a[i]);
+	printf("\n");
 }
