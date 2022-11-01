643492e82c174d5fbe20cc7807ea47bfe577400b
soooyaya <hsoo0531@naver.com>
2022-11-01 17:36:45 +0900

    05

--- main.c
+++ main.c
@@ -1,37 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 4
+#define ROWS 3
+#define COLS 3
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-void square_array(int a[], int size);
-void print_array(int a[], int size);
-
+int C[ROWS][COLS]; //덧셈 결과 행렬 
 
 int main(int argc, char *argv[]) {
 	
-	int list[SIZE]	= {1, 2, 3, 4};
+	int A[ROWS][COLS] = { //더하기 대상 행렬 
+	{2, 3, 0},
+	{8, 9, 1},
+	{7, 0, 5}};
 	
-	print_array(list, SIZE);
-	square_array(list, SIZE);
-	print_array(list, SIZE);
+	int B[ROWS][COLS] = {//더하기 대상 행렬 
+	{1, 0, 0},
+	{0, 1, 0},
+	{0, 0, 1}};
 	
+	addMatrix(A, B, C);
+	printMatrix(C);
+
 	return 0;
 }
 
-void square_array(int a[], int size)
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
 { 
-  int i;
-
-  for (i=0;i<size;i++)
-     a[i] = a[i]*a[i];
+   int i, j;  
+   for(i=0;i<ROWS;i++)
+     for(j=0;j<ROWS;j++)
+         C[i][j] = A[i][j] + B[i][j];
+    	  
 }
 
-void print_array(int a[], int size)
+void printMatrix(int A[][COLS])
 {
-	int i;
-	
-	for (i=0;i<size;i++)
-	  printf("%3d", a[i]);
-	printf("\n");
+	int i, j;
+	for(i=0;i<ROWS;i++)
+	  {
+	  	for(j=0;j<ROWS;j++)
+		printf("%d\t", C[i][j]);
+	    printf("\n");	//for 문을 이용해서 계산 
 }
+ } 
